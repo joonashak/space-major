b200d55aa8da003303bd614a09084ee15e15a220
joonashak <hakkijoo@gmail.com>
2023-01-14 12:39:37 +0200

    Add mongo

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MONGO_URL } from './config';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(MONGO_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
