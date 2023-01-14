7e34875e2d4f98f6cda0eb41faa1105dc4f79179
joonashak <hakkijoo@gmail.com>
2023-01-14 12:44:17 +0200

    Fix linting

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MONGO_URL } from './config';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MONGO_URL } from "./config";
 
 @Module({
   imports: [MongooseModule.forRoot(MONGO_URL)],
