66c71b394b74090877899bc8683f1c467cf50d9f
joonashak <joonashak@proton.me>
2024-01-07 18:41:30 +0200

    Clean up

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,36 +1,10 @@
 import { CssBaseline } from '@mui/joy';
 import Button from '@mui/joy/Button';
-import { useState } from 'react';
-import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <CssBaseline/>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <Button variant="solid">Hello world</Button>
     </>
   )
