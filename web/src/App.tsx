92914a66ac8850d4afccb9c9d7286064fe1fe239
joonashak <joonashak@proton.me>
2024-01-07 18:38:14 +0200

    Install Joy UI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { CssBaseline } from '@mui/joy';
+import Button from '@mui/joy/Button';
+import { useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <CssBaseline/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button variant="solid">Hello world</Button>
     </>
   )
 }
