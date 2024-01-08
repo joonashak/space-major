0f692cd155238d42606f331f33a453c543f84296
joonashak <joonashak@proton.me>
2024-01-08 22:10:27 +0200

    Add op selector

--- web/src/Navigator.tsx
+++ web/src/Navigator.tsx
@@ -1,12 +1,21 @@
 import InfoRoundedIcon from "@mui/icons-material/InfoRounded";
 import PolicyRoundedIcon from "@mui/icons-material/PolicyRounded";
 import SpaceDashboardRoundedIcon from "@mui/icons-material/SpaceDashboardRounded";
-import { Box, List, Typography } from "@mui/joy";
+import { Box, List, Option, Select, Typography } from "@mui/joy";
 import NavigationButton from "./NavigationButton";
 
 const Navigator = () => (
   <Box sx={{ width: 1 }}>
-    <Typography>TODO: Op Selector</Typography>
+    <Select
+      color="primary"
+      placeholder="Choose one…"
+      size="md"
+      variant="outlined"
+      sx={{ mt: 1, mb: 2 }}
+    >
+      <Option value={1}>Mega Op</Option>
+      <Option value={2}>Nice Op</Option>
+    </Select>
     <List sx={{ gap: 1 }}>
       <NavigationButton
         icon={<SpaceDashboardRoundedIcon />}
