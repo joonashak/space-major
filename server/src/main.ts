e0ab033405715f86432dd92a569aa38605a4df0e
joonashak <joonashak@proton.me>
2024-01-09 08:07:30 +0200

    Connect backend to mongo

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import mongoose from 'mongoose';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  mongoose.set('debug', true);
   await app.listen(3000);
 }
 bootstrap();
