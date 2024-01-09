1e600a4c482387d657e4e3bc7225dde0e4759d23
joonashak <joonashak@proton.me>
2024-01-09 18:43:18 +0200

    Setup GraphQL

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,24 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URL)],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      // FIXME: These settings cannot be used in production.
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: false,
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault({ includeCookies: true }),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
