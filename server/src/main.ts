f2b7c8b715416889785a339c8502e477fa71afed
joonashak <joonashak@proton.me>
2024-01-09 08:15:28 +0200

    Set up express-session

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import MongoStore from 'connect-mongo';
+import session from 'express-session';
 import mongoose from 'mongoose';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URL,
+      }),
+    }),
+  );
   mongoose.set('debug', true);
   await app.listen(3000);
 }
