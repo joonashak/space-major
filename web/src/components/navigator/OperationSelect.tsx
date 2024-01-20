084e025a48da4087fecc775a7384cdcfb0290242
joonashak <joonashak@proton.me>
2024-01-20 21:03:39 +0200

    Implement navigating between ops

--- web/src/components/navigator/OperationSelect.tsx
+++ web/src/components/navigator/OperationSelect.tsx
@@ -1,19 +1,36 @@
 import { useQuery } from "@apollo/client";
 import { Option, Select } from "@mui/joy";
-import { FindAllOperationsDocument } from "../../generated/graphql-operations";
+import { SyntheticEvent } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  FindAllOperationsDocument,
+  Operation,
+} from "../../generated/graphql-operations";
 
 const OperationSelect = () => {
   const { data } = useQuery(FindAllOperationsDocument);
+  const { opName } = useParams();
+  const navigate = useNavigate();
 
   const ops = data?.findAllOperations || [];
+  const selectedOp =
+    (ops.find((op) => op.shortName === opName) as Operation) || null;
+
+  const onChange = (_: SyntheticEvent | null, value: Operation | null) => {
+    if (value) {
+      navigate(`/op/${value.shortName}`);
+    }
+  };
 
   return (
     <Select
       color="primary"
-      placeholder="Choose one…"
+      placeholder="Select Operation..."
       size="md"
       variant="outlined"
       sx={{ mt: 1, mb: 2 }}
+      onChange={onChange}
+      value={selectedOp}
     >
       {ops.map((op) => (
         <Option value={op} key={op.id}>
